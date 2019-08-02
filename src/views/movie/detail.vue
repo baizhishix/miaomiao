e3242222da995d7b340fa9cc22bacc10e6ae756b
zhl@1202455500403538.onaliyun.com <1372632329@qq.com>
2019-08-03 01:14:47 +0800

    bug clear

--- src/views/movie/detail.vue
+++ src/views/movie/detail.vue
@@ -6,7 +6,7 @@
         <loader v-if="isLoading"></loader>
         <div v-else class="contentDetail">
             <div class="detail_list">
-                <div class="detail_list_bg"></div>
+                <div class="detail_list_bg" :style="{ 'background-image': 'url('+ detailMovie.img.replace(/w\.h/, '148.208') +')'}"></div>
                 <div class="detail_list_filter"></div>
                 <div class="detail_list_content">
                     <div class="detail_list_img">
@@ -98,7 +98,7 @@ export default {
             .detail_list_bg{
                 width: 100%;
                 height: 100%;
-                background: url(/images/movie_1.jpg) 0 40%;
+                background: 0 40%;
                 filter: blur(20px);
                 background-size: cover;
                 position: absolute;
